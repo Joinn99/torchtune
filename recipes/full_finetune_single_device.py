b491eb68c458c4d9cdb55b44aa664272c4154e32
Joinn99 <Joinnnor@outlook.com>
2025-07-06 08:37:10 +0000

    Model resize with config

--- recipes/full_finetune_single_device.py
+++ recipes/full_finetune_single_device.py
@@ -267,8 +267,8 @@ class FullFinetuneRecipeSingleDevice(FTRecipeInterface):
         self._tokenizer = config.instantiate(cfg.tokenizer)
         self._logger.info("Tokenizer is initialized from file.")
 
-        if cfg.get("resize_token_embeddings", False):
-            resize_token_embeddings(self._model, self._tokenizer.vocab_size)
+        if cfg.get("resize_token_embeddings", False) and cfg.get("resized_vocab_size", None) is not None:
+            resize_token_embeddings(self._model, cfg.get("resized_vocab_size", None))
 
         # _setup_optimizer should take in ckpt_dict only if training is resumed from
         # checkpoint. Transforming the opt state dict is handled by this method
